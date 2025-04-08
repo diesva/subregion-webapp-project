37df1b6f740653e72f758e85d5c790edd77fc46e
diesva <diegoesva04@gmail.com>
2025-04-08 16:10:16 -0500

    Added Perfil COnfiguration
    Added Perfil Config

--- subregion-webapp/src/components/templates/HomeTemplate.jsx
+++ subregion-webapp/src/components/templates/HomeTemplate.jsx
@@ -1,24 +1,64 @@
 import styled from "styled-components";
-import {Btnsave} from "../moleculas/Btnsave";
-import {useAuthStore} from "../../store/AuthStore"
+import { Header } from "../organismos/Header";
+import { useState } from "react";
 export function HomeTemplate() {
-    const {signOut} = useAuthStore();
+    const [state, setState] = useState(false);
     return (
         <Container>
-            <h1>UPLOAD SERVER - SUB REGION ILO</h1>
-            <Btnsave titulo="Cerrar sesion" bgcolor="fff" funcion={signOut}/>
+            <header className="header">
+                <Header
+                    stateConfig={{ state: state, setState:()=> setState (!state)}}
+                />    
+            </header>
+            <section className="area1">
+
+            </section>
+            <section className="area2">
+
+            </section>
+            <section className="main">
+
+            </section>
         </Container>
     );
 }
 
 // 🔹 Define el Container aquí
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    
     height: 100vh;
-    overflow: hidden;
+    width: 100%;
     background-color:${(props)=>props.theme.bgtotal};
     color: ${({theme})=>theme.text};
-    width: 100%;
+    display: grid;
+    padding:15px;
+    grid-template:
+        "header" 100px 
+        "area1" 100px
+        "area2" 100px
+        "main" auto;
+    .header{
+        grid-area:header;
+        background-color: rgba(103,93,241,0.14);
+        display: flex;
+        align-items: center;
+    }
+    .area1{
+        grid-area:area1;
+        background-color: rgba(229,67,26,0.14);
+        display: flex;
+        align-items: center;
+    }
+    .area2{
+        grid-area:area2;
+        background-color: rgba(77,237,106,0.14);
+        display: flex;
+        align-items: center;
+    }
+    .main{
+        grid-area:main;
+        background-color: rgba(179,46,241,0.14);
+        display: flex;
+        align-items: center;
+    }
 `;
